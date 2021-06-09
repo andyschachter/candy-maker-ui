ae4d1bcaa0825ef16017052f9236a2533876f923
Andy Schachter <andy.schachter@gmail.com>
2021-06-08 20:47:40 -0400

    added cors

--- candy-maker-app/index.js
+++ candy-maker-app/index.js
@@ -2,10 +2,19 @@ const express = require('express')
 const { getAllManufacturers, getManufacturerById } = require('./controllers/manufacturers')
 const { getAllProducts, getProductsById } = require('./controllers/products')
 const path = require('path')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.static('public'))
+
+app.use((req, res, next) => {
+  res.setHeader(
+    "content-security-policy-report-only",
+    "default-src 'self'; script-src 'report-sample' 'self'; style-src 'report-sample' 'self'; object-src 'none'; base-uri 'self'; connect-src 'self' https://api.github.com; font-src 'self'; frame-src 'self'; img-src 'self' https://*.githubusercontent.com; manifest-src 'self'; media-src 'self'; report-uri https://5f723390a82aebac90867ce8.endpoint.csper.io/; worker-src 'none';"
+  );
+  next();
+});
 
 app.get('/api/manufacturers', getAllManufacturers)
 
